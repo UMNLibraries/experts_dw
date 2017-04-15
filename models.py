0e4aea20b4217bfd9195a0e7961d7de7c22cbde3
David Naughton <naughton@umn.edu>
2017-04-15 13:46:45 -0500

    Removed emplid-based FK from mds_person_tenure_track_flag.

--- models.py
+++ models.py
@@ -138,11 +138,9 @@ class MdsPersonTenureFlag(Base):
 class MdsPersonTenureTrackFlag(Base):
   __tablename__ = 'mds_person_tenure_track_flag'
   tenure_track_flag = Column(String(1), nullable=True)
-  emplid = Column(ForeignKey('mds_person_emplid.emplid'), nullable=False)
-  uuid = Column(ForeignKey('mds_person.uuid'), nullable=False)
-  timestamp = Column(DateTime, default=func.current_timestamp(), nullable=False, primary_key=True)
+  uuid = Column(ForeignKey('mds_person.uuid'), primary_key=True)
+  timestamp = Column(DateTime, default=func.current_timestamp(), primary_key=True)
   mds_person = relationship('MdsPerson', cascade="all, delete-orphan")
-  mds_person_emplid = relationship('MdsPersonEmplid', cascade="all, delete-orphan")
 
   def __repr__(self):
     return 'tenure_track_flag: {}, uuid: {}, timestamp: {}'.format(self.tenure_track_flag, self.uuid, self.timestamp)
