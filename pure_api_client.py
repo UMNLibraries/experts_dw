729906240e5b4334de92ea9bf6996ca7b4a4e57f
David Naughton <naughton@umn.edu>
2017-04-19 19:20:59 -0500

    Improved the params for pure_api_client.get.

--- pure_api_client.py
+++ pure_api_client.py
@@ -8,9 +8,9 @@ pure_api_url = os.environ.get('PURE_API_URL')
 pure_api_user = os.environ.get('PURE_API_USER')
 pure_api_pass = os.environ.get('PURE_API_PASS')
 
-def get(family, window_size):
+def get(family, params={}):
   return requests.get(
     pure_api_url + family,
     auth=(pure_api_user, pure_api_pass),
-    params={'window.size': window_size, 'namespaces': 'remove'}
+    params=params
   )
