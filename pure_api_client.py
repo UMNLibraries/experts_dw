8a49aee45d0b0ba6ce48c3afb14480fdd9bedada
David Naughton <naughton@umn.edu>
2017-04-19 22:22:43 -0500

    Added pure_api_client.get_all().

--- pure_api_client.py
+++ pure_api_client.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv, find_dotenv
 import os
 import requests
+import math
+import xml.etree.ElementTree as et
 
 load_dotenv(find_dotenv())
 
@@ -14,3 +16,14 @@ def get(family, params={}):
     auth=(pure_api_user, pure_api_pass),
     params=params
   )
+  
+def get_all(family, params={}):
+  r = get(family, {"window.size": 1, "namespaces":"remove"})
+  xml = et.fromstring(r.text)
+  record_count = int(xml.find("count").text)
+  window_size = int(params['window.size']) if 'window.size' in params else 100
+  window_count = int(math.ceil(float(record_count) / window_size))
+
+  for window in range(0, window_count):
+     window_params = {'window.offset': window * window_size}
+     yield get(family, {**params, **window_params})
