619511602960b1a8fa223f8a8f690f4f71982bb1
David Naughton <naughton@umn.edu>
2017-04-28 18:38:04 -0500

    Added pure_api_response_parser.publication().

--- pure_api_response_parser.py
+++ pure_api_response_parser.py
@@ -19,3 +19,30 @@ def person(record):
   person['scopus_id'] = scopus_id_elem.attrib['source_id'] if scopus_id_elem is not None else None
 
   return person
+
+def publication(record):
+  publication = {  
+    'pure_uuid': record.attrib['uuid'],
+    'title': record.find('./title').text,
+  }
+
+  year = record.find('./publicationDate/year').text
+  issued_precision = 366
+  month = '01'
+  day = '01'
+  month_elem = record.find('./publicationDate/month')
+  if (month_elem is not None):
+    month = month_elem.text
+    if len(month) == 1:
+      month = '0' + month
+    issued_precision = 31
+  day_elem = record.find('./publicationDate/day')
+  if (day_elem is not None):
+    day = day_elem.text
+    if len(day) == 1:
+      day = '0' + day
+    issued_precision = 1
+  publication['issued'] = '-'.join([year, month, day])
+  publication['issued_precision'] = issued_precision
+
+  return publication
