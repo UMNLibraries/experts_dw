7caf74205be1b1aae7443b3b3e87b15e2edb52c2
David Naughton <naughton@umn.edu>
2017-05-05 10:07:17 -0500

    Added another note about a constraint violation.

--- delete_internal_orgs_lacking_pure_uuids.py
+++ delete_internal_orgs_lacking_pure_uuids.py
@@ -11,6 +11,9 @@ session = db.session('hotel')
 # umn_dept_id: 10773, umn_dept_name: AES Ag Services, pure_org_uuid: None, pure_org_id: ZNVNNMA
 # umn_dept_id: 10764, umn_dept_name: AES Administration Operations, pure_org_uuid: None, pure_org_id: ZNVNNMA
 
+# NOTE: Also deleted this from umn_person_pure_org, because we have no pure_org with pure_org_id NNIWQIQRQ:
+# person_uuid: 089e447f-54f1-40d4-b79a-911f3c788dc9, emplid: 2109835, pure_org_id: NNIWQIQRQ, job_description: 'Assistant Director Cmp/Col', employed_as: Academic, staff_type: nonacademic, start_date: 20-JUL-15, end_date: 16-NOV-15, pure_person_id: 3467    
+
 # id  pure_id     type        name_en                                  lft parent_id level rft tree_id
 # 271 NNIWQIQRQ   department  Minnesota Agriculture Experiment Station 577 24        3     578 1   
 # 24  ZNVNNMA     college     Minnesota Agriculture Experiment Station 576 1         2     579 1   
