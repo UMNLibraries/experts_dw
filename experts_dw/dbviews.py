376d6654bedf126cc30459183b926da6e6ddf3b3
David Naughton <naughton@umn.edu>
2017-12-21 18:33:17 -0600

    Fixes pure_eligible_affiliates, adds pure_eligible_persons.

--- experts_dw/dbviews.py
+++ experts_dw/dbviews.py
@@ -2,6 +2,85 @@ from . import db
 
 session = db.session('hotel')
 
+def create_pure_eligible_persons():
+  stmt = """
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "EXPERT"."PURE_ELIGIBLE_PERSONS" (
+  "EMPLID",
+  "NAME"
+) AS (
+  select emplid, name
+  from pure_eligible_affiliates
+  union
+  select emplid, name
+  from pure_eligible_employees
+)"""
+  #print(stmt)
+  result = session.execute(stmt)
+  session.commit()
+  return result
+
+def create_pure_eligible_affiliates():
+  stmt = """
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "EXPERT"."PURE_ELIGIBLE_AFFILIATES" (
+  "EMPLID",
+  "NAME",
+  "JOBCODE",
+  "DEPTID",
+  "DEPTID_DESCR",
+  "UM_COLLEGE",
+  "UM_COLLEGE_DESCR",
+  "CAMPUS",
+  "STATUS_FLG"
+) AS (
+  select
+    emplid,
+    name, -- for testing
+    um_affil_relation as jobcode,
+    deptid,
+    deptid_descr,
+    um_college,
+    um_college_descr,
+    um_campus as campus,
+    status_flg
+ from ps_dwhr_um_affiliates@dweprd.oit a
+ where poi_type = '00012'
+   and status = 'A'
+   and status_flg = 'C'
+   and (
+     (
+       um_affil_relation in -- jobcode criteria table
+       (select jobcode from job_codes where pool = 'A')
+     ) or (
+       um_affil_relation in -- only affiliates in designated jobcodes AND departments
+       (select jobcode from job_codes where pool = 'C')
+       and deptid in (select deptid from affiliate_departments)
+     )
+   )
+   and um_campus in (
+     'TXXX',
+     'DXXX'
+   )
+   and um_college not in (
+     'TATH',
+     'TAUD',
+     'TAUX',
+     'TBOY',
+     'TCAP',
+     'TCTR',
+     'TFAC',
+     'TOGC',
+     'THSM',
+     'TINS',
+     'TOBR',
+     'TOHR',
+     'TSVC'
+   )
+)"""
+  #print(stmt)
+  result = session.execute(stmt)
+  session.commit()
+  return result
+
 def create_pure_eligible_employees():
   stmt = """
 CREATE OR REPLACE FORCE EDITIONABLE VIEW "EXPERT"."PURE_ELIGIBLE_EMPLOYEES" (
--- tests/test_dbviews.py
+++ tests/test_dbviews.py
@@ -3,8 +3,16 @@ load_dotenv(find_dotenv())
 from experts_dw import dbviews
 from sqlalchemy.engine import ResultProxy
 
+def test_pure_eligible_affiliates():
+  result = dbviews.create_pure_eligible_affiliates()
+  assert isinstance(result, ResultProxy)
+
 def test_pure_eligible_employees():
   result = dbviews.create_pure_eligible_employees()
+  assert isinstance(result, ResultProxy)
+
+def test_pure_eligible_persons():
+  result = dbviews.create_pure_eligible_persons()
   assert isinstance(result, ResultProxy)
 
 def test_employee_jobs_current():
