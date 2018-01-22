fbe4101cf9818409c9236dd01ef3e3b0f92d1527
David Naughton <naughton@umn.edu>
2018-01-22 12:01:39 -0600

    Possibly unnecessary alembic fix for class name PureEligibleDemogChngHst.

---
+++ alembic/versions/10fc42d665cc_fixes_class_name_.py
@@ -0,0 +1,22 @@
+"""Fixes class name PureEligibleDemogChngHst.
+
+Revision ID: 10fc42d665cc
+Revises: 7f7265c52336
+Create Date: 2018-01-22 11:58:08.447663
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import oracle
+
+# revision identifiers, used by Alembic.
+revision = '10fc42d665cc'
+down_revision = '7f7265c52336'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+  pass
+
+def downgrade():
+  pass
