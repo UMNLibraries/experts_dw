5c573377820e306e3a2ec3f04cfa2508dec64198
David Naughton <naughton@pop-os.localdomain>
2020-07-22 09:13:32 -0500

    Adds a statement to delete obsolete primary jobs. Fixes #26.

---
+++ experts_dw/sql/delete_obsolete_primary_jobs.sql
@@ -0,0 +1,8 @@
+DELETE FROM pure_sync_staff_org_association ps
+WHERE ps.primary_association = 1
+AND ps.modified < (
+  SELECT MAX(ps2.modified)
+  FROM pure_sync_staff_org_association ps2
+  WHERE ps2.primary_association = 1
+  AND ps2.person_id = ps.person_id
+)
