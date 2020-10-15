c8457a83aca982f793993a36abddf364fa07d67f
David Naughton <naughton@pop-os.localdomain>
2020-10-15 14:33:16 -0500

    Fixes and improves table exclusions for alembic migrations.

--- alembic/env.py
+++ alembic/env.py
@@ -36,64 +36,65 @@ target_metadata = models.Base.metadata
 
 # Added for Experts@Minnesota to support config via env vars.
 def get_url():
-   return db.url('hotel')
+    return db.url('hotel')
 
 exclude_tables = config.get_section('alembic:exclude').get('tables', '').split(',')
 
 def include_object(object, name, type_, reflected, compare_to):
-  if type_ == "table" and name in exclude_tables:
-    return False
-  else:
-    return True
+    if type_ == "table" and name in exclude_tables:
+        return False
+    else:
+        return True
 
 def run_migrations_offline():
-  """Run migrations in 'offline' mode.
+    """Run migrations in 'offline' mode.
 
-  This configures the context with just a URL
-  and not an Engine, though an Engine is acceptable
-  here as well.  By skipping the Engine creation
-  we don't even need a DBAPI to be available.
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
 
-  Calls to context.execute() here emit the given string to the
-  script output.
+    Calls to context.execute() here emit the given string to the
+    script output.
 
-  """
+    """
 
-  # Modified for Experts@Minnesota to support config via env vars.
-  url = get_url()
-  context.configure(
-    url=url,
-    target_metadata=target_metadata,
-    include_object=include_object,
-    literal_binds=True,
-    compare_type=True
-  )
-
-  with context.begin_transaction():
-    context.run_migrations()
-
-def run_migrations_online():
-  """Run migrations in 'online' mode.
-
-  In this scenario we need to create an Engine
-  and associate a connection with the context.
-
-  """
-
-  # Modified for Experts@Minnesota to support config via env vars.
-  connectable = create_engine(get_url(),max_identifier_length=128)
-
-  with connectable.connect() as connection:
+    # Modified for Experts@Minnesota to support config via env vars.
+    url = get_url()
     context.configure(
-      connection=connection,
-      target_metadata=target_metadata,
-      compare_type=True
+        url=url,
+        target_metadata=target_metadata,
+        include_object=include_object,
+        literal_binds=True,
+        compare_type=True
     )
 
     with context.begin_transaction():
-      context.run_migrations()
+        context.run_migrations()
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+
+    # Modified for Experts@Minnesota to support config via env vars.
+    connectable = create_engine(get_url(),max_identifier_length=128)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            include_object=include_object,
+            compare_type=True
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
 
 if context.is_offline_mode():
-  run_migrations_offline()
+    run_migrations_offline()
 else:
-  run_migrations_online()
+    run_migrations_online()
