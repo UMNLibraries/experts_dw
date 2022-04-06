6765cbb2105e314692a1fd5147789c0da7030fbd
David Naughton <naughton@pop-os.localdomain>
2022-04-06 14:20:30 -0500

    Modifies db views tests to use sqlalchemy ^1.4.

--- tests/test_dbviews.py
+++ tests/test_dbviews.py
@@ -1,6 +1,6 @@
 import pytest
 from sqlalchemy import func
-from sqlalchemy.engine import ResultProxy
+from sqlalchemy.engine.cursor import CursorResult
 from experts_dw import db, dbviews
 from experts_dw.models import PureEligibleDemographics, PureEligiblePerson, PureEligibleAffiliateJob, PureEligibleEmployeeJob, PureEligiblePOIJob
 
@@ -16,7 +16,7 @@ def test_list_view_creation_functions():
 def test_create_all_views(session):
   results = dbviews.create_all_views(session)
   for fn in dbviews._view_creation_functions():
-    assert isinstance(results[fn], ResultProxy)
+    assert isinstance(results[fn], CursorResult)
 
 # Verify views with backing models return rows
 def test_view_rows(session):
