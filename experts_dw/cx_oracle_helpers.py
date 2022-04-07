4619522618cb7070921cf117a675af472db5b221
John Barneson <barne102@umn.edu>
2022-04-07 13:40:27 -0500

    Initial commit of cx_oracle_helpers and unit test functions

--- experts_dw/cx_oracle_helpers.py
+++ experts_dw/cx_oracle_helpers.py
@@ -15,5 +15,18 @@ def select_list_of_dicts(cursor, sql, params=None):
     cursor.rowfactory = lambda *args: dict(
         zip([col[0] for col in cursor.description], args)
     )
+    result = cursor.fetchall() # Result will be a list of dicts
+    if bool(result) is False: # Empty list is False
+        return None
+    else:
+        return result
+
+def select_list_of_scalars(cursor, sql, params=None):
+    if params is None:
+        params = {}
+    cursor.execute(sql, params)
     result = cursor.fetchall()
-    return result
+    if bool(result) is False:
+        return None
+    else:
+        return result
