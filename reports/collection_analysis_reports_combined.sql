d3788f84bbc0874a623bf49f8e90abb49d07242a
David Naughton <naughton@pop-os.localdomain>
2025-04-08 13:25:51 -0500

    Reduces collections analysis reports down to 2 queries

--- reports/collection_analysis_reports_combined.sql
+++ reports/collection_analysis_reports_combined.sql
@@ -1,98 +1,23 @@
--- Collections Analysis Reports Combined
--- SQL for creating temporary tables and running two final reports:
--- 1. UMN Authored Articles by ISSN
--- 2. UMN Cited Articles by ISSN
--- Assumes the availability of scopus_json_abstract_authored and scopus_json_abstract_cited tables.
+-- New collections analysis reports:
 
--- Temp Table 1
--- umn_cited_abstract_ids
--- Contains cited abstract scopus id, publication year, issn, and doi.
--- DROP TABLE umn_cited_abstract_ids;
+-- Collections Analysis Report 1
+-- UMN Authored Abstracts by ISSN
+--DROP TABLE collections_analysis_authored;
 
-CREATE TABLE umn_cited_abstract_ids AS
-    SELECT
-      sjc.scopus_id AS cited_abstract_scopus_id,
-      sjson.cited_abstract_year,
-      sjson.issn AS cited_abstract_issn,
-      sjson.doi AS cited_abstract_doi
-    FROM
-      scopus_json_abstract_cited sjc,
-      JSON_TABLE (
-        sjc.json_document, '$."abstracts-retrieval-response"'
-        COLUMNS (
-          cited_abstract_year PATH '$.item.bibrecord.head.source.publicationyear."@first"',
-          issn PATH '$.item.bibrecord.head.source.issn[0]."$"',
-          doi PATH '$.coredata."prism:doi"'
-        )
-      ) sjson
-;
-
--- Temp Table 2
--- umn_authored_abstract_cited_scopus_ids
--- Two column table matching the authored_abstract to its cited_abstracts
---DROP TABLE umn_authored_abstract_cited_scopus_ids;
-
-CREATE TABLE umn_authored_abstract_cited_scopus_ids AS
-SELECT
-      sja.scopus_id as authored_abstract_scopus_id,
-      sjson.cited_abstract_scopus_id
-    FROM
-      scopus_json_abstract_authored sja,
-      JSON_TABLE (
-        sja.json_document, '$."abstracts-retrieval-response"'
-          COLUMNS (
-            NESTED PATH '$.item.bibrecord.tail.bibliography.reference[*]' COLUMNS (
-              NESTED PATH '$."ref-info"."refd-itemidlist".itemid[*]' COLUMNS (
-                cited_abstract_scopus_id_type PATH '$."@idtype"',
-                cited_abstract_scopus_id PATH '$."$"'
-              )
-            )
-          )
-      ) sjson
-    WHERE sjson.cited_abstract_scopus_id_type = 'SGR'
-;
-
--- Temp Table 3
--- umn_authored_abstract_author_scopus_ids
--- Two column table matching the abstract's scopus id with an author's scopus id
---DROP TABLE umn_authored_abstract_author_scopus_ids;
-
-CREATE TABLE umn_authored_abstract_author_scopus_ids AS
-    SELECT
-      sja.scopus_id as authored_abstract_scopus_id,
-      sjson.author_scopus_id
-    FROM
-      scopus_json_abstract_authored sja,
-      JSON_TABLE (
-        sja.json_document, '$."abstracts-retrieval-response"'
-        COLUMNS (
-          NESTED PATH '$.authors.author[*]' COLUMNS (
-            author_scopus_id PATH '$."@auid"'
-          )
-        )
-      ) sjson
-;
-
--- Temp Table 4
--- UMN_PURE_PERSON_IDS
--- Matches author scopus id to emplid, internet_id, first_name, last_name
---DROP TABLE UMN_PURE_PERSON_IDS;
-
-CREATE TABLE UMN_PURE_PERSON_IDS AS
- SELECT DISTINCT
-    pspd.emplid,
-    pspd.internet_id,
-    pspd.first_name,
-    pspd.last_name,
-    sjson.author_id AS author_scopus_id
-  FROM
-    pure_json_person_524 pjp,
+CREATE TABLE collections_analysis_authored AS
+WITH author_metadata AS (
+  SELECT DISTINCT
+     pspd.emplid,
+     pspd.internet_id,
+     pspd.first_name,
+     pspd.last_name,
+     sjson.scopus_id 
+  FROM pure_json_person_524 pjp,
     JSON_TABLE (
-      pjp.json_document, '$'
-      COLUMNS (
+      pjp.json_document, '$' COLUMNS (
         external_id PATH '$.externalId',
         NESTED PATH '$.ids[*]' COLUMNS (
-          author_id PATH '$.value.value',
+          scopus_id PATH '$.value.value',
           NESTED PATH '$.type.term.text[*]' COLUMNS (
             author_id_type PATH '$.value'
           )
@@ -100,102 +25,83 @@ CREATE TABLE UMN_PURE_PERSON_IDS AS
       )
     ) sjson
   JOIN pure_sync_person_data pspd
-  ON sjson.external_id = pspd.person_id
+    ON sjson.external_id = pspd.person_id
   WHERE sjson.author_id_type = 'Scopus Author ID'
-  ;
-
--- Temp Table 5
--- umn_author_ids
--- Matches the authored abstract scopus id to author scopus id and author metadata (emplid, internet_id, f/l name)
---DROP TABLE umn_author_ids;
-
-CREATE TABLE umn_author_ids AS
-    SELECT
-      asi.authored_abstract_scopus_id,
-      uppi.author_scopus_id,
-      uppi.emplid,
-      uppi.internet_id,
-      uppi.first_name,
-      uppi.last_name
-    FROM umn_authored_abstract_author_scopus_ids asi
-    JOIN umn_pure_person_ids uppi
-    ON asi.author_scopus_id = uppi.author_scopus_id
-;
-
--- Collection Analysis Report 1
--- U of MN Authored Abstracts by ISSN
--- This code needs updating to reflect new metadata and temp tables.
---DROP TABLE umn_scopus_articles_authors_by_issn;
-
-CREATE TABLE umn_scopus_articles_authors_by_issn
-AS
-WITH
-scopus_ids AS (
-    SELECT
-        sjson.doi as DOI,
-        sjson.issn as ISSN,
-        sja.scopus_id as ABSTRACT_SCOPUS_ID,
-        sjson.author_scopus_id as AUTHOR_SCOPUS_ID,
-        sjson.abstract_year AS ABSTRACT_YEAR
-    FROM
-        scopus_json_abstract_authored sja,
-        JSON_TABLE(sja.json_document, '$."abstracts-retrieval-response"'
-            COLUMNS (
-                doi PATH '$.coredata."prism:doi"',
-                issn    PATH '$.item.bibrecord.head.source.issn[0]."$"',
-                abstract_year PATH '$.item.bibrecord.head.source.publicationyear."@first"',
-                NESTED PATH '$.item.bibrecord.head."author-group"[*]' COLUMNS(
-                    NESTED PATH '$.author[*]' COLUMNS(author_scopus_id PATH '$."@auid"'))
-            )) sjson
-    ORDER BY sjson.issn
 )
-
 SELECT
-  si.issn as ISSN,
-  si.doi as ARTICLE_DOI,
-  si.abstract_scopus_id,
-  si.abstract_year,
-  si.author_scopus_id,
-  uai.emplid as EMPLID,
-  uai.internet_id,
-  uai.last_name,
-  uai.first_name
-FROM scopus_ids si
-JOIN umn_author_ids uai
-ON (
-  si.author_scopus_id = uai.author_scopus_id AND
-  si.abstract_scopus_id = uai.authored_abstract_scopus_id
-)
-ORDER BY si.issn
+  sja.scopus_id AS abstract_scopus_id,
+  sjson.doi,
+  sjson.issn,
+  sjson.publication_year,
+  sjson.author_scopus_id,
+  am.emplid,
+  am.internet_id,
+  am.first_name,
+  am.last_name
+FROM scopus_json_abstract_authored sja,
+  JSON_TABLE (
+    sja.json_document, '$."abstracts-retrieval-response"' COLUMNS (
+      doi PATH '$.coredata."prism:doi"',
+      issn PATH '$.item.bibrecord.head.source.issn[0]."$"',
+      publication_year PATH '$.item.bibrecord.head.source.publicationyear."@first"',
+      NESTED PATH '$.authors.author[*]' COLUMNS (
+        author_scopus_id PATH '$."@auid"'
+      )
+    )
+  ) sjson
+JOIN author_metadata am
+  ON sjson.author_scopus_id = am.scopus_id
 ;
 
 -- Collection Analysis Report 2
--- U of MN Cited Abstracts by ISSN
---DROP TABLE umn_scopus_cited_articles_authors_by_issn;
+-- UMN Cited Abstracts by ISSN
+--DROP TABLE collections_analysis_cited;
 
-CREATE TABLE umn_scopus_cited_articles_authors_by_issn AS
-
+CREATE TABLE collections_analysis_cited AS
+WITH cited_abstract_metadata AS (
+  SELECT
+    sjc.scopus_id AS cited_abstract_scopus_id,
+    sjson.*
+  FROM
+    scopus_json_abstract_cited sjc,
+    JSON_TABLE (
+      sjc.json_document, '$."abstracts-retrieval-response"' COLUMNS (
+        cited_abstract_doi PATH '$.coredata."prism:doi"',
+        cited_abstract_issn PATH '$.item.bibrecord.head.source.issn[0]."$"',
+        cited_abstract_publication_year PATH '$.item.bibrecord.head.source.publicationyear."@first"'
+      )
+    ) sjson
+), authored_cited_scopus_ids AS (
+  SELECT
+    sja.scopus_id as authored_abstract_scopus_id,
+    sjson.cited_abstract_scopus_id
+  FROM scopus_json_abstract_authored sja,
+    JSON_TABLE (
+      sja.json_document, '$."abstracts-retrieval-response"' COLUMNS ( 
+        NESTED PATH '$.item.bibrecord.tail.bibliography.reference[*]' COLUMNS (
+          NESTED PATH '$."ref-info"."refd-itemidlist".itemid[*]' COLUMNS (
+            cited_abstract_scopus_id_type PATH '$."@idtype"',
+            cited_abstract_scopus_id PATH '$."$"'
+          )
+        )
+      )
+    ) sjson
+  WHERE sjson.cited_abstract_scopus_id_type = 'SGR'
+)
 SELECT
-  ci.cited_abstract_issn,
-  ci.cited_abstract_doi,
-  ci.cited_abstract_year,
-  csi.cited_abstract_scopus_id,
-  uai.authored_abstract_scopus_id,
-  uai.author_scopus_id,
-  uai.emplid,
-  uai.internet_id,
-  uai.first_name,
-  uai.last_name
-FROM umn_author_ids uai
-JOIN umn_authored_abstract_cited_scopus_ids csi
-ON uai.authored_abstract_scopus_id = csi.authored_abstract_scopus_id
-JOIN umn_cited_abstract_ids ci
-ON csi.cited_abstract_scopus_id = ci.cited_abstract_scopus_id
+  cam.cited_abstract_scopus_id,
+  cam.cited_abstract_doi,
+  cam.cited_abstract_issn,
+  cam.cited_abstract_publication_year,
+  caa.abstract_scopus_id AS authored_abstract_scopus_id,
+  caa.author_scopus_id,
+  caa.emplid,
+  caa.internet_id,
+  caa.first_name,
+  caa.last_name
+FROM cited_abstract_metadata cam
+JOIN authored_cited_scopus_ids acsi
+  ON cam.cited_abstract_scopus_id = acsi.cited_abstract_scopus_id
+JOIN collections_analysis_authored caa
+  ON caa.abstract_scopus_id = acsi.authored_abstract_scopus_id
 ;
-
-
-
-
-
-
-
