6b7acb9f8fbe7c6306067b86303c71d53a255278
David Naughton <nihiliad@gmail.com>
2025-07-14 18:22:39 -0500

    Pulls Scopus API collection metadata into its own class for DRYer validation

--- experts_dw/scopus_json.py
+++ experts_dw/scopus_json.py
@@ -3,235 +3,20 @@ import functools
 import json
 from typing import Any, Callable, MutableMapping, Tuple, TypeVar, cast
 
+import cx_Oracle
+
 from experts_dw import db
 from experts_dw.exceptions import ExpertsDwException
+from experts_dw.scopus_json_collection_meta import CollectionMeta
 
-iso_8601_format = '%Y-%m-%dT%H:%M:%S.%f%z'
-
-F = TypeVar('F', bound=Callable[..., Any])
-
-@functools.lru_cache(maxsize=None)
-def collection_local_names(cursor):
-    cursor.execute(
-        'SELECT DISTINCT(local_name) FROM scopus_json_collection_meta',
-    )
-    return [row[0] for row in cursor.fetchall()]
-
-@functools.lru_cache(maxsize=None)
-def collection_local_name_for_api_name(cursor, *, collection_api_name):
-    cursor.execute(
-        'SELECT local_name FROM scopus_json_collection_meta WHERE api_name = :api_name',
-        {'api_name': collection_api_name}
-    )
-    result = cursor.fetchone()
-    if result is None:
-        return result
-    else:
-        return result[0] # Result will be a tuple
-
-@functools.lru_cache(maxsize=None)
-def collection_local_name_for_schema_record_name(cursor, *, collection_schema_record_name):
-    cursor.execute(
-        'SELECT local_name FROM scopus_json_collection_meta WHERE schema_record_name = :schema_record_name',
-        {'schema_record_name': collection_schema_record_name}
-    )
-    result = cursor.fetchone()
-    if result is None:
-        return result
-    else:
-        return result[0] # Result will be a tuple
-
-@functools.lru_cache(maxsize=None)
-def collection_schema_record_name_for_local_name(cursor, *, collection_local_name):
-    cursor.execute(
-        'SELECT schema_record_name FROM scopus_json_collection_meta WHERE local_name = :local_name',
-        {'local_name': collection_local_name}
-    )
-    result = cursor.fetchone()
-    if result is None:
-        return result
-    else:
-        return result[0] # Result will be a tuple
-
-@functools.lru_cache(maxsize=None)
-def collection_api_name_for_local_name(cursor, *, collection_local_name):
-    cursor.execute(
-        'SELECT api_name FROM scopus_json_collection_meta WHERE local_name = :local_name',
-        {'local_name': collection_local_name}
-    )
-    result = cursor.fetchone()
-    if result is None:
-        return result
-    else:
-        return result[0] # Result will be a tuple
-
-class MissingCollectionName(ValueError, ExpertsDwException):
-    '''Raised when a Scopus collection name is expected but missing.'''
-    def __init__(self, *args, **kwargs):
-        super().__init__(
-            'No collection_local_name, collection_api_name, or collection_schema_record_name found in kwargs',
-            *args,
-            **kwargs
-        )
-
-class InvalidCollectionLocalName(ValueError, ExpertsDwException):
-    '''Raised when a local collection name is invalid.'''
-    def __init__(self, *args, collection_local_name, **kwargs):
-        super().__init__(
-            f'Invalid collection_local_name "{collection_local_name}"',
-            *args,
-            **kwargs
-        )
-
-class InvalidCollectionApiName(ValueError, ExpertsDwException):
-    '''Raised when a Scopus API collection name is invalid.'''
-    def __init__(self, *args, collection_api_name, **kwargs):
-        super().__init__(
-            f'Invalid collection_api_name "{collection_api_name}"',
-            *args,
-            **kwargs
-        )
-
-class InvalidCollectionSchemaRecordName(ValueError, ExpertsDwException):
-    '''Raised when a Scopus API schema record name is invalid.'''
-    def __init__(self, *args, collection_schema_record_name, **kwargs):
-        super().__init__(
-            f'Invalid collection_schema_record_name "{collection_schema_record_name}"',
-            *args,
-            **kwargs
-        )
-
-def validate_collection_names(func: F) -> F:
-    '''A decorator wrapper that ensures that collection_local_name,
-    collection_api_name, and collection_schema_record_name all exist in kwargs, are valid,
-    and consistent with each other.
-
-    Args:
-        func: The function to be wrapped.
-
-    Return:
-        The wrapped function.
-
-    Raises:
-        MissingCollectionName: If none of the various ``collectiion_*`` names is in kwargs.
-        InvalidCollectionLocalName: If the ``collection_local_name`` is not found
-        InvalidCollectionApiName: If the ``collection_api_name`` is not found
-        InvalidCollectionSchemaRecordName: If the ``collection_schema_record_name`` is not found
-    '''
-    @functools.wraps(func)
-    def wrapper_validate_collection_names(*args, **kwargs):
-        cursor = args[0]
-        if 'collection_local_name' in kwargs and kwargs['collection_local_name'] is not None:
-            if kwargs['collection_local_name'] not in collection_local_names(cursor):
-                raise InvalidCollectionLocalName(
-                    collection_local_name=kwargs['collection_local_name']
-                )
-        elif 'collection_api_name' in kwargs and kwargs['collection_api_name'] is not None:
-            kwargs['collection_local_name'] = collection_local_name_for_api_name(
-                cursor,
-                collection_api_name=kwargs['collection_api_name']
-            )
-            if kwargs['collection_local_name'] is None:
-                raise InvalidCollectionApiName(
-                    collection_api_name=kwargs['collection_api_name']
-                )
-        elif 'collection_schema_record_name' in kwargs and kwargs['collection_schema_record_name'] is not None:
-            kwargs['collection_local_name'] = collection_local_name_for_schema_record_name(
-                cursor,
-                collection_schema_record_name=kwargs['collection_schema_record_name']
-            )
-            if kwargs['collection_local_name'] is None:
-                raise InvalidCollectionSchemaRecordName(
-                    collection_schema_record_name=kwargs['collection_schema_record_name']
-                )
-        if ('collection_local_name' not in kwargs) or (kwargs['collection_local_name'] is None):
-            raise MissingCollectionName()
-
-        kwargs['collection_schema_record_name'] = collection_schema_record_name_for_local_name(
-            cursor,
-            collection_local_name=kwargs['collection_local_name']
-        )
-        kwargs['collection_api_name'] = collection_api_name_for_local_name(
-            cursor,
-            collection_local_name=kwargs['collection_local_name']
-        )
-        return func(*args, **kwargs)
-    return cast(F, wrapper_validate_collection_names)
-
-class MissingRelation(ValueError, ExpertsDwException):
-    '''Raised when a relation is expected but missing.'''
-    def __init__(self, *args, **kwargs):
-        super().__init__(
-            'No relation found in kwargs',
-            *args,
-            **kwargs
-        )
-
-class InvalidRelation(ValueError, ExpertsDwException):
-    '''Raised when a relation is invalid.'''
-    def __init__(self, *args, relation, **kwargs):
-        super().__init__(
-            f'Invalid relation "{relation}"',
-            *args,
-            **kwargs
-        )
-
-def validate_relation(func: F) -> F:
-    '''A decorator wrapper that ensures that relation has a valid value, either ``authored`` or ``cited``.
-
-    Args:
-        func: The function to be wrapped.
-
-    Return:
-        The wrapped function.
-
-    Raises:
-        InvalidRelation: If the ``relation`` is invalid
-        MissingRelation: If the ``relation`` is missing
-    '''
-    @functools.wraps(func)
-    def wrapper_validate_relation(*args, **kwargs):
-        if 'relation' in kwargs:
-            kwargs['relation'] = kwargs['relation'].lower()
-            if kwargs['relation'] not in ['authored', 'cited']:
-                raise InvalidRelation(
-                    relation=kwargs['relation']
-                )
-        else:
-            raise MissingRelation()
-        return func(*args, **kwargs)
-    return cast(F, wrapper_validate_relation)
-
-# Notice that this function has no validation. We recommend calling it only
-# from other functions in this module that do have parameter validation.
-def get_collection_table_name(
-    *,
-    collection_local_name,
-    relation='authored',
-    staging=False
-):
-    collection_table_name = f'scopus_json_{collection_local_name}_{relation}'
-    if staging:
-        return collection_table_name + '_staging'
-    return collection_table_name
-
-@validate_collection_names
-@validate_relation
 def document_exists(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    scopus_id,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
-    staging=False
+    scopus_id:str,
+    meta:CollectionMeta,
+    staging:bool=False
 ):
-    collection_table_name = get_collection_table_name(
-        collection_local_name=collection_local_name,
-        relation=relation,
-        staging=staging
-    )
+    collection_table_name = meta.staging_table_name if staging else meta.canonical_table_name
     cursor.execute(
         f'SELECT count(*) FROM {collection_table_name} WHERE scopus_id = :scopus_id',
         {'scopus_id': scopus_id}
@@ -242,22 +27,13 @@ def document_exists(
     else:
         return False
 
-@validate_collection_names
-@validate_relation
 def insert_sql(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
-    staging=False
+    meta:CollectionMeta,
+    staging:bool=False
 ):
-    collection_table_name = get_collection_table_name(
-        collection_local_name=collection_local_name,
-        relation=relation,
-        staging=staging
-    )
+    collection_table_name = meta.staging_table_name if staging else meta.canonical_table_name
     primary_key_column_names = 'scopus_id'
     primary_key_predicate = 'scopus_id = :scopus_id'
     if staging:
@@ -283,89 +59,53 @@ def insert_sql(
         )
     '''
 
-@validate_collection_names
-@validate_relation
 def insert_document(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    document,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
-    staging=False
+    document:MutableMapping,
+    meta:CollectionMeta,
+    staging:bool=False
 ):
     sql = insert_sql(
         cursor,
-        collection_local_name=collection_local_name,
-        relation=relation,
+        meta=meta,
         staging=staging
     )
     cursor.execute(sql, document)
 
-@validate_collection_names
-@validate_relation
 def insert_documents(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    documents,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
-    staging=False
+    documents:list[MutableMapping],
+    meta:CollectionMeta,
+    staging:bool=False
 ):
     sql = insert_sql(
         cursor,
-        collection_local_name=collection_local_name,
-        relation=relation,
+        meta=meta,
         staging=staging
     )
     cursor.executemany(sql, documents)
 
-@validate_collection_names
-@validate_relation
 def truncate_staging(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
+    meta:CollectionMeta,
 ):
-    collection_staging_table_name = get_collection_table_name(
-        collection_local_name=collection_local_name,
-        relation=relation,
-        staging=True
-    )
-    cursor.execute(f'TRUNCATE TABLE {collection_staging_table_name}')
+    cursor.execute(f'TRUNCATE TABLE {meta.staging_table_name}')
 
-@validate_collection_names
-@validate_relation
 def merge_documents_from_staging_sql(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
+    meta:CollectionMeta,
 ):
-    collection_table_name = get_collection_table_name(
-        collection_local_name=collection_local_name,
-        relation=relation,
-    )
-    collection_staging_table_name = get_collection_table_name(
-        collection_local_name=collection_local_name,
-        relation=relation,
-        staging=True,
-    )
     return f'''
-        MERGE INTO {collection_table_name} sj
+        MERGE INTO {meta.canonical_table_name} sj
         USING (
           SELECT scopus_id, inserted, json_document, updated, scopus_created, scopus_modified FROM ( 
             SELECT scopus_id, inserted, json_document, updated, scopus_created, scopus_modified,
               RANK() OVER (PARTITION BY scopus_id ORDER BY scopus_modified DESC) latest
-              FROM {collection_staging_table_name}
+              FROM {meta.staging_table_name}
           ) where latest = 1
         ) sjs
         ON (sjs.scopus_id = sj.scopus_id)
@@ -381,32 +121,21 @@ def merge_documents_from_staging_sql(
           VALUES (sjs.scopus_id, sjs.inserted, sjs.json_document, sjs.updated, sjs.scopus_created, sjs.scopus_modified)
     '''
 
-@validate_collection_names
-@validate_relation
 def merge_documents_from_staging(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
+    meta:CollectionMeta,
 ):
     sql = merge_documents_from_staging_sql(
         cursor,
-        collection_local_name=collection_local_name,
-        relation=relation,
+        meta=meta,
     )
     cursor.execute(sql)
 
-@validate_collection_names
-@validate_relation
 def load_documents_from_staging(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
+    meta:CollectionMeta,
 ):
     connection = cursor.connection
     connection.begin()
@@ -414,14 +143,12 @@ def load_documents_from_staging(
     try:
         merge_documents_from_staging(
             cursor,
-            collection_local_name=collection_local_name,
-            relation=relation,
+            meta=meta,
         )
 
         truncate_staging(
             cursor,
-            collection_local_name=collection_local_name,
-            relation=relation,
+            meta=meta,
         )
 
     except Exception as e:
@@ -430,42 +157,32 @@ def load_documents_from_staging(
 
     connection.commit()
 
-@validate_collection_names
-@validate_relation
 def delete_documents_matching_scopus_ids_sql(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    scopus_ids,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
+    scopus_ids:list[str],
+    meta:CollectionMeta,
+    # Should we support this for staging tables, too?
 ):
     collection_table_name = get_collection_table_name(
         collection_local_name=collection_local_name,
-        relation=relation,
     )
     bind_vars = ','.join(f':{i}' for i in range(len(scopus_ids)))
     return f'''
-        DELETE FROM {collection_table_name}
+        DELETE FROM {meta.canonical_table_name}
         WHERE scopus_id IN ({bind_vars})
     '''
 
-@validate_collection_names
-@validate_relation
 def delete_documents_matching_scopus_ids(
-    cursor,
+    cursor:cx_Oracle.Cursor,
     *,
-    scopus_ids,
-    collection_local_name=None,
-    collection_api_name=None,
-    collection_schema_record_name=None,
-    relation='authored',
+    scopus_ids:list[str],
+    meta:CollectionMeta,
+    # Should we support this for staging tables, too?
 ):
     sql = delete_documents_matching_scopus_ids_sql(
         cursor,
         scopus_ids=scopus_ids,
-        collection_local_name=collection_local_name,
-        relation=relation,
+        meta=meta,
     )
     cursor.execute(sql, scopus_ids)
---
+++ tests/test_scopus_json_collection_meta.py
@@ -0,0 +1,80 @@
+import dotenv_switch.auto
+
+from attrs import asdict
+import pytest
+
+from experts_dw import db
+from experts_dw.scopus_json_collection_meta import \
+    CollectionMeta, \
+    InvalidCollectionLocalName, \
+    InvalidCollectionApiName, \
+    InvalidCollectionSchemaRecordName, \
+    get_collection_meta_by_local_name, \
+    get_collection_meta_by_api_name, \
+    get_collection_meta_by_schema_record_name \
+
+@pytest.fixture
+def connection():
+    with db.cx_oracle_connection() as connection:
+        yield connection
+
+@pytest.fixture
+def cursor(connection):
+    return connection.cursor()
+
+def test_get_collection_meta_by_local_name(cursor):
+    meta = get_collection_meta_by_local_name(
+        cursor=cursor,
+        local_name='abstract',
+    )
+    assert isinstance(meta, CollectionMeta)
+    assert asdict(meta) == {
+        'local_name': 'abstract',
+        'api_name': 'abstract',
+        'schema_record_name': 'abstracts-retrieval-response',
+        'canonical_table_name': 'scopus_json_abstract',
+        'staging_table_name': 'scopus_json_abstract_staging',
+    } 
+    with pytest.raises(InvalidCollectionLocalName):
+        meta = get_collection_meta_by_local_name(
+            cursor=cursor,
+            local_name='bogus',
+        )
+
+def test_get_collection_meta_by_api_name(cursor):
+    meta = get_collection_meta_by_api_name(
+        cursor=cursor,
+        api_name='abstract/citations',
+    )
+    assert isinstance(meta, CollectionMeta)
+    assert asdict(meta) == {
+        'local_name': 'citation',
+        'api_name': 'abstract/citations',
+        'schema_record_name': 'abstract-citations-response',
+        'canonical_table_name': 'scopus_json_citation',
+        'staging_table_name': 'scopus_json_citation_staging',
+    }
+    with pytest.raises(InvalidCollectionApiName):
+        meta = get_collection_meta_by_api_name(
+            cursor=cursor,
+            api_name='bogus',
+        )
+
+def test_get_collection_meta_by_schema_record_name(cursor):
+    meta = get_collection_meta_by_schema_record_name(
+        cursor=cursor,
+        schema_record_name='abstracts-retrieval-response',
+    )
+    assert isinstance(meta, CollectionMeta)
+    assert asdict(meta) == {
+        'local_name': 'abstract',
+        'api_name': 'abstract',
+        'schema_record_name': 'abstracts-retrieval-response',
+        'canonical_table_name': 'scopus_json_abstract',
+        'staging_table_name': 'scopus_json_abstract_staging',
+    }
+    with pytest.raises(InvalidCollectionSchemaRecordName):
+        meta = get_collection_meta_by_schema_record_name(
+            cursor=cursor,
+            schema_record_name='bogus',
+        )
