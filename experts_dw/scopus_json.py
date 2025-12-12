2c01c237af381bdfb389242e78a271c4b93b1590
David Naughton <nihiliad@gmail.com>
2025-12-12 09:10:08 -0600

    Adds scopus_json.update_abstract_to_download(), improves some table aliases

--- experts_dw/scopus_json.py
+++ experts_dw/scopus_json.py
@@ -5,7 +5,7 @@ from typing import Any, Callable, Iterable, MutableMapping, Tuple, TypeVar, cast
 
 import cx_Oracle
 
-from experts_dw import db
+from experts_dw import db, pure_json_collection_meta
 from experts_dw.exceptions import ExpertsDwException
 from experts_dw.scopus_json_collection_meta import CollectionMeta
 
@@ -137,25 +137,25 @@ def merge_documents_from_staging_sql(
     meta:CollectionMeta,
 ):
     return f'''
-        MERGE INTO {meta.canonical_table_name} sj
+        MERGE INTO {meta.canonical_table_name} canonical
         USING (
           SELECT scopus_id, inserted, json_document, updated, scopus_created, scopus_modified FROM ( 
             SELECT scopus_id, inserted, json_document, updated, scopus_created, scopus_modified,
               RANK() OVER (PARTITION BY scopus_id ORDER BY scopus_modified DESC) latest
               FROM {meta.staging_table_name}
-          ) where latest = 1
-        ) sjs
-        ON (sjs.scopus_id = sj.scopus_id)
+          ) WHERE latest = 1
+        ) staging
+        ON (staging.scopus_id = canonical.scopus_id)
         WHEN MATCHED
           THEN UPDATE SET
-            sj.scopus_modified = sjs.scopus_modified,
-            sj.updated = sjs.updated,
-            sj.json_document = sjs.json_document
+            canonical.scopus_modified = staging.scopus_modified,
+            canonical.updated         = staging.updated,
+            canonical.json_document   = staging.json_document
           WHERE
-            sjs.scopus_modified > sj.scopus_modified
+            staging.scopus_modified > canonical.scopus_modified
         WHEN NOT MATCHED
-          THEN INSERT (sj.scopus_id, sj.inserted, sj.json_document, sj.updated, sj.scopus_created, sj.scopus_modified)
-          VALUES (sjs.scopus_id, sjs.inserted, sjs.json_document, sjs.updated, sjs.scopus_created, sjs.scopus_modified)
+          THEN INSERT (canonical.scopus_id, canonical.inserted, canonical.json_document, canonical.updated, canonical.scopus_created, canonical.scopus_modified)
+          VALUES (staging.scopus_id, staging.inserted, staging.json_document, staging.updated, staging.scopus_created, staging.scopus_modified)
     '''
 
 def merge_documents_from_staging(
@@ -299,3 +299,77 @@ def insert_scopus_ids_to_download(
             for scopus_id in scopus_ids
         ],
     )
+
+def update_abstract_to_download_sql(
+    cursor:cx_Oracle.Cursor,
+    *,
+    # Maybe create something like the following, to make the types more specific?
+    #abstract_meta:AbstractCollectionMeta,
+    abstract_meta:CollectionMeta,
+    citation_meta:CollectionMeta,
+    pure_ro_meta:pure_json_collection_meta.CollectionMeta,
+    past_months_limit:int=36, # Past 36 months, 3 years is our default reporting window
+):
+    return f'''
+        MERGE INTO {abstract_meta.to_download_table_name} to_download
+        USING (
+          WITH scopus_id_to_exclude AS (
+            SELECT scopus_id FROM {abstract_meta.defunct_table_name}
+            UNION
+            SELECT scopus_id FROM {citation_meta.defunct_table_name}
+          )
+          SELECT
+            rojt.scopus_id
+          FROM
+            -- pure json research output:
+            {pure_ro_meta.canonical_table_name} ro,
+            JSON_TABLE(ro.json_document, '$'
+              COLUMNS (
+                scopus_id       PATH '$.externalId',
+                external_source PATH '$.externalIdSource',
+                ro_type         PATH '$.type.uri',
+                NESTED PATH '$.publicationStatuses[*]'
+                  COLUMNS (
+                    ro_year    PATH '$.publicationDate.year',
+                    ro_current PATH '$.current',
+                    ro_status  PATH '$.publicationStatus.uri'
+                  )
+              )) rojt
+          LEFT JOIN scopus_id_to_exclude exclude
+            ON rojt.scopus_id = exclude.scopus_id
+          WHERE exclude.scopus_id IS NULL
+            AND rojt.external_source = 'Scopus'
+            AND rojt.ro_type LIKE '/dk/atira/pure/researchoutput/researchoutputtypes/contributiontojournal/%'
+            AND rojt.ro_current = 'true'
+            AND rojt.ro_status = '/dk/atira/pure/researchoutput/status/published'
+            AND TO_DATE(rojt.ro_year, 'YYYY') >= ADD_MONTHS(SYSDATE, - {past_months_limit})
+            AND (
+              -- This includes all the new and modified articles...
+              ro.pure_modified >= (SELECT MAX(scopus_modified) FROM {abstract_meta.canonical_table_name})
+              OR
+              -- ...while this includes any older articles missing from scopus_json_abstract:
+              rojt.scopus_id NOT IN (SELECT scopus_id FROM {abstract_meta.canonical_table_name})
+            )
+        ) pure_ro
+        ON (pure_ro.scopus_id = to_download.scopus_id)
+        WHEN NOT MATCHED THEN
+          INSERT (to_download.scopus_id, to_download.inserted)
+          VALUES (pure_ro.scopus_id, SYSDATE)
+    '''
+
+def update_abstract_to_download(
+    cursor:cx_Oracle.Cursor,
+    *,
+    abstract_meta:CollectionMeta,
+    citation_meta:CollectionMeta,
+    pure_ro_meta:pure_json_collection_meta.CollectionMeta,
+    past_months_limit:int=36, # Past 36 months, 3 years is our default reporting window
+):
+    sql = update_abstract_to_download_sql(
+        cursor,
+        abstract_meta=abstract_meta,
+        citation_meta=citation_meta,
+        pure_ro_meta=pure_ro_meta,
+        past_months_limit=past_months_limit,
+    )
+    cursor.execute(sql)
